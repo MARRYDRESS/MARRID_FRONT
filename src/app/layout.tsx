66f6cf0a92b3639540dd7a96611d8f5d883d6f50
안민주 <anminju0310@gmail.com>
2026-05-14 10:23:50 +0900

    feat :: 렌더링 시 부드럽게 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import GlobalStyle from "@/src/style/globalStyle";
 import StyledComponentsRegistry from "@/src/lib/styledComponentsRegistry";
+import PageTransition from "@/src/components/layout/pageTransition";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <StyledComponentsRegistry><GlobalStyle />{children}</StyledComponentsRegistry>
+        <StyledComponentsRegistry>
+          <GlobalStyle />
+          <PageTransition>{children}</PageTransition>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
